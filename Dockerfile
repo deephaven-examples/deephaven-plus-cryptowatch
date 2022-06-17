3a36f1032859ac5516e7268199fa1996ad660051
Jake Mulford <jakemulf@gmail.com>
2022-06-17 14:02:24 -0400

    minor deploy fixes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM ghcr.io/deephaven/server:edge
 COPY requirements.txt /requirements.txt
 RUN pip install -r /requirements.txt
-COPY app.d /app.d
+COPY data/app.d /app.d
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-docker build --tag deephaven-examples/deephaven-plus-cryptowatch
+docker build --tag deephaven-examples/deephaven-plus-cryptowatch .
 docker compose up --build
